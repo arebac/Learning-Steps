6032f17df1b8d2cbfd2006c2ceb007f9702b38c0
arebac <j.cabreraortiz@outlook.com>
2024-03-14 06:44:23 -0400

    Learning State

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import Steps from "./components/Steps";
+import "./index.css";
+
+function App() {
+
+  {
+    return (
+      <div>
+      <Steps/>
+      <Steps/>
+      </div>
+        )
+    }
+}
+export default App;
